aa47247770c0f994b58b8cb11673b27d985a90e6
蒋龙生 <jianglongsheng@cmiot.chinamobile.com>
2018-10-26 10:52:58 +0800

    1

--- xyx-poker-pdk/src/main/java/com/xyx/poker/pdk/modal/Poker.java
+++ xyx-poker-pdk/src/main/java/com/xyx/poker/pdk/modal/Poker.java
@@ -3,5 +3,34 @@ package com.xyx.poker.pdk.modal;
 public class Poker {
 	private String name;
 	private Integer value;
-	private PokerColor color;
+	private Integer color;
+	
+	public Poker(String name){
+		this(name,null);
+	}
+			
+	public Poker(String name,Integer color){
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getValue() {
+		return value;
+	}
+	public void setValue(Integer value) {
+		this.value = value;
+	}
+	public Integer getColor() {
+		return color;
+	}
+	public void setColor(Integer color) {
+		this.color = color;
+	}
+	
+	
 }
